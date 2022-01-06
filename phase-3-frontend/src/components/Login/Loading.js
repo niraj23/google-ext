3efdb3dccb0cf0ae182117664c9551d0311aab22
niraj23 <Niraj2323@gmail.com>
2022-01-06 17:42:15 -0500

    Delete phase-3-frontend directory

--- phase-3-frontend/src/components/Login/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
-import '../../loading.css'
-
-const Loading = () => (
-    <ReactLoading id='load' type='spinningbubbles' color='red' height={'20%'} width={'20%'} />
-);
-
-export default Loading;
