140e306a100169604d12487c05a326862b494050
Niraj <niraj2323@gmail.com>
2022-01-13 10:15:27 -0500

    Completed

--- Portfolio-Niraj/src/components/Resume/Resume.js
+++ Portfolio-Niraj/src/components/Resume/Resume.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://docdro.id/fw3VMAT";
+  "https://raw.githubusercontent.com/niraj23/Portfolio/main/Portfolio-Niraj/src/Assets/resume.pdf";
 
 function Resume() {
   const [width, setWidth] = useState(1200);
